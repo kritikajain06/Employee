0d2c22bec0a4bd16acec735918997915db98ddab
kritikajain06 <kritika06jain@gmail.com>
2019-10-29 02:29:34 -0700

    main

---
+++ MyMainUsingEmployee.java
@@ -0,0 +1,47 @@
+package com.sid.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyMainUsingEmployee {
+	public static void main(String args[]) {
+		List<Employee> employeeList = new ArrayList<>();
+		employeeList.add(new Employee(101, "sid", "IT", "Developer", 48843));
+		employeeList.add(new Employee(102, "vineet", "IT", "Developer", 54443));
+		employeeList.add(new Employee(103, "shourya", "IT", "Developer", 59823));
+		employeeList.add(new Employee(104, "surya", "sales", "Manager", 84533));
+		employeeList.add(new Employee(105, "abhi", "sales", "Salesman", 56763));
+		employeeList.add(new Employee(106, "vijay", "HR", "Manager", 98533));
+		employeeList.add(new Employee(106, "vishwas", "HR", "Manager", 33233));
+		employeeList.add(new Employee(107, "keshav", "HR", "Senior", 22333));
+		employeeList.add(new Employee(107, "suraj", "HR", "Senior", 76333));
+		
+		employeeList.stream()
+		.filter(employee->employee.getDept().equals("HR"))
+		.map(Employee::getName)
+		.sorted()
+		.forEach(System.out::println);
+		
+		employeeList.stream()
+		.filter(employee->employee.getDept().equals("sales"))
+		.map(employee->{
+			double salary = employee.getSalary()*1.1;
+			employee.setSalary(salary);
+			return employee;
+		}).forEach(System.out::println);
+		
+		System.out.println("--------------------");
+		
+		
+		employeeList.stream()
+		.filter(employee->employee.getDept().equals("HR"))
+		.filter(employee->employee.getDesig().equals("Manager"))
+		.map(employee->{
+			employee.setSalary(employee.getSalary()*1.25);
+			employee.setDesig("Senior Manager");
+			return employee;
+		}).forEach(System.out::println);
+		
+		System.out.println("--------------------");
+	}
+}
